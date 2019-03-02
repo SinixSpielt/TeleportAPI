2b05791abc4e63774c712ff7d794485f95638c04
SinixSpielt <marvinhaenel@gmx.net>
2019-03-02 08:09:08 +0100

    Update TeleportAPI.java

--- TeleportAPI/src/de/sinixspielt/teleportapi/TeleportAPI.java
+++ TeleportAPI/src/de/sinixspielt/teleportapi/TeleportAPI.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.sinixspielt.teleportapi.file.FileManager;
-import de.sinixspielt.teleportapi.listeners.MoveListener;
 import de.sinixspielt.teleportapi.manager.TeleportManager;
 
 public class TeleportAPI extends JavaPlugin{
@@ -18,7 +17,6 @@ public class TeleportAPI extends JavaPlugin{
 		instance = this;
 		fileManager = new FileManager();
 		teleportManager = new TeleportManager();
-		Bukkit.getPluginManager().registerEvents(new MoveListener(), this);
 	}
 
 	@Override
